bd69437aca40ff2493872529b6a7f2e4d213e6d3
PepeGF <gran.pepe@gmail.com>
2025-01-21 22:44:29 +0100

    feat(setup): Add db persistency, fixed minor issue django apps, created simple dummy model, tz set

--- be/apps/core/admin.py
+++ be/apps/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from apps.core.models import Users
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'email')
+	search_fields = ('nombre', 'email')
+
+admin.site.register(Users, UserAdmin)
--- be/config/settings.py
+++ be/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'apps.core'
 ]
 
 MIDDLEWARE = [
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
