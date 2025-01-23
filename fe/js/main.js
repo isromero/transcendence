a132c5422b7af0e18d8cd3e4f1b27ee5307a1d8f
amsteradri <amsteradri@gmail.com>
2025-01-23 20:45:34 +0100

    feat(comments): erased comments + forgot to mention new icon logic on js

--- fe/js/main.js
+++ fe/js/main.js
@@ -1,6 +1,5 @@
 const appContainer = document.getElementById('app-container');
 
-// Función para cargar una página
 async function loadPage(page) {
   try {
     const url = page === '/' ? '/index.html' : `/pages${page}.html`;
@@ -11,44 +10,44 @@ async function loadPage(page) {
     const content = await response.text();
     appContainer.innerHTML = content;
 
-    // Cambia el estado del historial
+
     window.history.pushState({ page }, '', page);
 
-    // Actualiza los iconos después de cargar la nueva página
+
     updateIcons(page);
   } catch (error) {
     appContainer.innerHTML = '<div class="text-center"><h1>Error 404</h1><p>Página no encontrada.</p></div>';
   }
 }
 
-// Función para actualizar los iconos según la página
+
 function updateIcons(page) {
   const leftButton = document.querySelector('.btn.position-absolute.bottom-0.start-0 i');
   const rightButton = document.querySelector('.btn.position-absolute.bottom-0.end-0 i');
 
   if (page === '/login') {
-    // Cambiar iconos para la página "friends"
-    leftButton.className = 'bi bi-question-circle'; // Cambia a un icono de perfil
+
+    leftButton.className = 'bi bi-question-circle';
     rightButton.className = 'bi bi-globe'; 
   } else if (page === '/register') {
-    // Cambiar iconos para la página "register"
-    leftButton.className = 'bi bi-question-circle'; // Cambia a un icono de perfil
+    
+    leftButton.className = 'bi bi-question-circle';
     rightButton.className = 'bi bi-globe'; 
   }
   else if (page === '/') {
-    // Cambiar iconos para la página "register"
-    leftButton.className = 'bi bi-question-circle'; // Cambia a un icono de perfil
+    
+    leftButton.className = 'bi bi-question-circle';
     rightButton.className = 'bi bi-globe'; 
   }
    else {
-    // Iconos por defecto
-    leftButton.className = 'bi bi-person-circle'; // Cambia a un icono de perfil
-    leftButton.parentElement.setAttribute('href', '/profile'); // Establece el href a /profile
+
+    leftButton.className = 'bi bi-person-circle';
+    leftButton.parentElement.setAttribute('href', '/profile');
     rightButton.className = 'bi bi-list'; 
   }
 }
 
-// Event listener para enlaces SPA
+
 document.body.addEventListener('click', (event) => {
   const link = event.target.closest('a.spa-link');
   if (link) {
@@ -58,11 +57,11 @@ document.body.addEventListener('click', (event) => {
   }
 });
 
-// Manejar el evento "popstate"
+
 window.addEventListener('popstate', (event) => {
   const page = event.state?.page || '/';
   loadPage(page);
 });
 
-// Cargar la página inicial
+
 loadPage(window.location.pathname);
