2bcababcf6b7cb80d18bf1e1558639f4701abe75
PepeGF <garcia.delafuente.jose@gmail.com>
2025-01-26 11:59:08 +0100

    feat(intial-data) : added frinendship do initial data

--- be/apps/core/admin.py
+++ be/apps/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.core.models import Users
+from apps.core.models import Users, Friends, Stats, Tournaments, History
 
 # Register your models here.
 
@@ -9,4 +9,34 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ("id", "username", "email")
 
 
+class FriendAdmin(admin.ModelAdmin):
+    list_display = ("id", "user_id", "friend_id", "status")
+
+
+class StatsAdmin(admin.ModelAdmin):
+    list_display = ("id", "user_id", "victories",
+                    "defeats", "total_matches",
+                    "total_tournaments", "tournaments_victories", )
+
+
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = (
+                    "id",
+                    "tournament_name",
+                    "start_date",
+                    "end_date",
+                    # "players",
+                    )
+
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "user_id", "result_user", "opponent",
+                    "result_opponent", "type_match", "tournament_id",
+                    "position_match", "date", "position_tournament",)
+
+
 admin.site.register(Users, UserAdmin)
+admin.site.register(Friends, FriendAdmin)
+admin.site.register(Stats, StatsAdmin)
+admin.site.register(Tournaments, TournamentAdmin)
+admin.site.register(History, HistoryAdmin)
