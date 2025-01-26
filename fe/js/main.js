508186c5adc300cfe308ee53e94b3bfbfb7f3be1
Tagamydev <samusanc@student.42madrid.com>
2025-01-26 16:47:37 +0100

    feat(fe files):
    general improvement to fe logic

--- fe/js/main.js
+++ fe/js/main.js
@@ -1,26 +1,121 @@
 const appContainer = document.getElementById('app-container');
+const pageContainer = document.getElementById('page-container');
+const modalContainer = document.getElementById('modal-container');
+const menuContainer = document.getElementById('menu-container');
 
 async function loadPage(page) {
+	let url;
+
+	if (page.includes("menu-"))
+	{
+		url = '/pages/menus${page}.html';
+		loadMenu(url);
+	}
+	else if (page.includes("game-"))
+	{
+		url = '/pages/game${page}.html';
+		loadGame(url);
+	}
+	else if (page.includes("modal-"))
+	{
+		url = '/components${page}.html';
+		loadModal(url);
+	}
+	else
+	{
+		url = '/components${page}.html';
+		loadError(url);
+	}
+}
+
+async function loadMenu(page) {
   try {
-    const url = page === '/' ? '/index.html' : `/pages${page}.html`;
+	const url = page;
     const response = await fetch(url);
 
-    if (!response.ok) throw new Error('Página no encontrada');
+	if (!response.ok)
+	{
+		/* TODO(samusanc): here goes an error checker for debug and other stuff, throw 
+		 * exception is a mistake!!!*/
+		throw new Error('Menu page not found');
+	}
+
+    const content = await response.text();
+    pageContainer.innerHTML = content;
+
+    //window.history.pushState({ page }, '', page);
+    //updateIcons(page);
+
+  } catch (error) {
+    pageContainer.innerHTML = '<div class="text-center"><h1>Unknow Error 404</h1><p>'error.message'</p></div>';
+  }
+}
+
+async function loadGame(page) {
+  try {
+	const url = page;
+    const response = await fetch(url);
+
+	if (!response.ok)
+	{
+		/* TODO(samusanc): here goes an error checker for debug and other stuff, throw 
+		 * exception is a mistake!!!*/
+		throw new Error('Game page not found');
+	}
 
     const content = await response.text();
     appContainer.innerHTML = content;
 
-
-    window.history.pushState({ page }, '', page);
-
-
-    updateIcons(page);
   } catch (error) {
-    appContainer.innerHTML = '<div class="text-center"><h1>Error 404</h1><p>Página no encontrada.</p></div>';
+	loadPage('/');
+    pageContainer.innerHTML = '<div class="text-center"><h1>Unknow Error 404</h1><p>'error.message'</p></div>';
   }
 }
 
+async function loadModal(page) {
+  try {
+	const url = page;
+    const response = await fetch(url);
 
+	if (!response.ok)
+	{
+		/* TODO(samusanc): here goes an error checker for debug and other stuff, throw 
+		 * exception is a mistake!!!*/
+		throw new Error('Modal page not found');
+	}
+
+    const content = await response.text();
+    modalContainer.innerHTML = content;
+
+  } catch (error) {
+    modalContainer.innerHTML = '<div class="text-center"><h1>Unknow Error 404</h1><p>'error.message'</p></div>';
+  }
+}
+
+async function loadError(page) {
+  try {
+    const url = page;
+    const response = await fetch(url);
+
+	if (!response.ok)
+	{
+		/* TODO(samusanc): here goes an error checker for debug and other stuff, throw 
+		 * exception is a mistake!!!*/
+		throw new Error('Error page not found');
+	}
+
+    const content = await response.text();
+    pageContainer.innerHTML = content;
+
+    //window.history.pushState({ page }, '', page);
+    //updateIcons(page);
+
+  } catch (error) {
+    pageContainer.innerHTML = '<div class="text-center"><h1>Unknow Error 404</h1><p>'error.message'</p></div>';
+  }
+}
+
+/*
 function updateIcons(page) {
   const leftButton = document.querySelector('.btn.position-absolute.bottom-0.start-0 i');
   const rightButton = document.querySelector('.btn.position-absolute.bottom-0.end-0 i');
@@ -47,22 +142,20 @@ function updateIcons(page) {
     rightButton.parentElement.setAttribute('href', '/settings');
   }
 }
-
+*/
 
 document.body.addEventListener('click', (event) => {
   const link = event.target.closest('a.spa-link');
   if (link) {
     event.preventDefault();
-    const page = link.getAttribute('href');
+    const page = link.getAttribute('lsp-loader');
     loadPage(page);
   }
 });
-
 
 window.addEventListener('popstate', (event) => {
   const page = event.state?.page || '/';
   loadPage(page);
 });
-
 
 loadPage(window.location.pathname);
