5b695c7a8349868cf71557d0ac86cf2861168932
PepeGF <gran.pepe@gmail.com>
2025-02-04 19:43:50 +0100

    feat(monitoring) - WIP Connection with backend OK. Data not persistant

--- be/config/settings.py
+++ be/config/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+	"django_prometheus",
     "apps.core",
 ]
 
 MIDDLEWARE = [
+	"django_prometheus.middleware.PrometheusBeforeMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -54,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+	"django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
