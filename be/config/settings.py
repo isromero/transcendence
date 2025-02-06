eebdf852d05b91239750f5d118ea607ed270ef6a
isromero <ismaromeroortega@gmail.com>
2025-02-06 18:50:50 +0100

    Add authentication middleware

--- be/config/settings.py
+++ be/config/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "apps.core.middleware.auth.AuthenticationMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
