3bd5ce3d39c591c28cd4c2b13d807125fe2b7d7f
Tagamydev <samusanc@student.42madrid.com>
2025-03-22 18:18:33 +0100

    fix(gdpr)

---
+++ fe/public/js/pages/privacy.js
@@ -0,0 +1,10 @@
+export async function changeLanguages() {
+    const lang = document.documentElement.lang;
+    const es = document.getElementById("en");
+    const en = document.getElementById("es");
+    const ua = document.getElementById("ua");
+  
+    if (lang === 'es')
+  }
+
+await changeLanguages();
