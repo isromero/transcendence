13f8dcf3c98e4571c31e92281b033d175eb35c11
amsteradri <amsteradri@gmail.com>
2025-04-12 18:48:11 +0200

    fix(profile stats + responsive)

--- fe/public/js/pages/profile.js
+++ fe/public/js/pages/profile.js
@@ -8,9 +8,10 @@ export function init() {
   const wins = document.getElementById('wins');
   const loses = document.getElementById('loses');
   const total = document.getElementById('total');
+  const tournamentWins = document.getElementById('tournament-wins'); // Nuevo
+  const tournamentLoses = document.getElementById('tournament-loses'); // Nuevo
 
   async function loadProfileData() {
-    // Get the ID of the URL if it exists
     const pathParts = window.location.pathname.split('/');
     const userId = pathParts[2]; // /profile/123 -> 123
 
@@ -18,15 +19,12 @@ export function init() {
       ? await usersService.getUser(userId)
       : await profileService.getProfile();
 
-    // Update avatar
     avatar.src = data.avatar
       ? `${IMAGES_URL}${data.avatar.replace('/images/', '/')}`
       : `${IMAGES_URL}/default_avatar.webp`;
 
-    // Update username
     username.textContent = data.username;
 
-    // Calculate statistics
     const victories = Number(data.victories || 0);
     const tournamentVictories = Number(data.tournament_victories || 0);
     const defeats = Number(data.defeats || 0);
@@ -34,13 +32,14 @@ export function init() {
     const totalMatches = Number(data.total_matches || 0);
     const totalTournaments = Number(data.total_tournaments || 0);
 
-    // Update statistics
-    wins.textContent = victories + tournamentVictories;
-    loses.textContent = defeats + tournamentDefeats;
-    total.textContent = totalMatches + totalTournaments;
+    wins.textContent = victories;
+    loses.textContent = defeats;
+    total.textContent = totalMatches;
+    tournamentWins.textContent = tournamentVictories; // Nuevo
+    tournamentLoses.textContent = tournamentDefeats; // Nuevo
   }
 
   loadProfileData();
 
-  return () => {};
+  return () => {};  return () => {};
 }
